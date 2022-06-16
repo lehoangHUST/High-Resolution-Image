008e493755061130ebbfc2c51c387a619359ee00
lehoangHUST <lehoangEDABK@gmail.com>
2022-06-17 02:35:00 +0700

    Update code

---
+++ datasets/data.py
@@ -0,0 +1,56 @@
+import cv2
+import os
+import pathlib
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class SRDataset(Dataset):
+    def __init__(self, path: str, task: str = 'train'):
+        super(SRDataset, self).__init__()
+        self.task = task
+        self.path = path
+        self.path_HR = os.path.join(self.path, self.task, 'HR')
+        self.path_LR = os.path.join(self.path, self.task, 'LR')
+
+    def get_csv(self):
+        csv_path = os.path.join(self.path, f"{self.task}.csv")
+        try:
+            dataframe = pd.read_csv(csv_path)
+        except FileNotFoundError:
+            column_name = ["filename HighResolution", "filename LowResolution"]
+            value_list = []
+            for i in range(len(self)):
+                imgname_HR, imgname_LR = os.listdir(self.path_LR)[i], os.listdir(self.path_HR)[i]
+                value = (os.path.join(self.path_HR, imgname_HR),
+                         os.path.join(self.path_LR, imgname_LR))
+                value_list.append(value)
+            dataframe = pd.DataFrame(value_list, columns=column_name)
+            dataframe.to_csv(csv_path, index=None)
+            print('Successfully created the CSV file: {}'.format(csv_path))
+            dataframe = pd.read_csv(csv_path)
+        return dataframe
+
+    def __getitem__(self, idx):
+        dataframe = self.get_csv()
+        imgname_HR, imgname_LR = dataframe.iloc[idx, 0], dataframe.iloc[idx, 1]
+        imgHR, imgLR = cv2.imread(imgname_HR), cv2.imread(imgname_LR)
+        # Convert
+        imgHR, imgLR = imgHR[:, :, ::-1], imgLR[:, :, ::-1]
+        imgHR, imgLR = imgHR.transpose(2, 0, 1), imgLR.transpose(2, 0, 1)  # (C,H,W)
+        imgHR, imgLR = np.ascontiguousarray(imgHR), np.ascontiguousarray(imgLR)
+        sample = {'LR_imagename': imgname_LR,
+                  'HR_imagename': imgname_HR,
+                  'LR_img': torch.from_numpy(imgLR),
+                  'HR_img': torch.from_numpy(imgHR)}
+        sample["LR_img"] = transforms.Resize((384, 384))(sample["LR_img"])
+        return sample
+
+    def __len__(self):
+        return len(os.listdir(self.path_LR))
+
---
+++ train.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+import copy
+
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms
+from tqdm import tqdm
+
+from models.SRCNN import SRCNN
+from datasets.data import SRDataset
+from utils.loss import AverageMeter, calc_psnr
+
+
+def run(args):
+    cudnn.benchmark = True
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = SRCNN(in_channels=3).to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam([
+        {'params': model.conv1.parameters()},
+        {'params': model.conv2.parameters()},
+        {'params': model.conv3.parameters(), 'lr': args.lr * 0.1}
+    ], lr=args.lr)
+
+    train_dataset = SRDataset(path=args.path, task='train')
+    train_dataloader = DataLoader(dataset=train_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  num_workers=args.num_workers,
+                                  pin_memory=True,
+                                  drop_last=True)
+    test_dataset = SRDataset(path=args.path, task='test')
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=1)
+
+    best_weights = copy.deepcopy(model.state_dict())
+    best_epoch = 0
+    best_psnr = 0.0
+
+    for epoch in range(args.num_epochs):
+        model.train()
+        epoch_losses = AverageMeter()
+
+        with tqdm(total=(len(train_dataset) - len(train_dataset) % args.batch_size)) as t:
+            t.set_description('epoch: {}/{}'.format(epoch, args.num_epochs - 1))
+
+            for sample in train_dataloader:
+                inputs = sample['LR_img'].type(torch.FloatTensor).to(device)
+                labels = sample['HR_img'].type(torch.FloatTensor).to(device)
+
+                preds = model(inputs)
+
+                loss = criterion(preds, labels)
+
+                epoch_losses.update(loss.item(), len(inputs))
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                t.set_postfix(loss='{:.6f}'.format(epoch_losses.avg))
+                t.update(len(inputs))
+
+        #torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch)))
+
+        model.eval()
+        epoch_psnr = AverageMeter()
+
+        for sample in test_dataloader:
+            inputs = sample['LR_img'].type(torch.FloatTensor)
+            labels = sample['HR_img'].type(torch.FloatTensor)
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            with torch.no_grad():
+                preds = model(inputs)
+
+            epoch_psnr.update(calc_psnr(preds, labels), len(inputs))
+
+        print('eval psnr: {:.2f}'.format(epoch_psnr.avg))
+
+        if epoch_psnr.avg > best_psnr:
+            best_epoch = epoch
+            best_psnr = epoch_psnr.avg
+            best_weights = copy.deepcopy(model.state_dict())
+
+    print('best epoch: {}, psnr: {:.2f}'.format(best_epoch, best_psnr))
+    #torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default=None)
+    parser.add_argument('--save_weights', type=str, default=None)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--num-epochs', type=int, default=100)
+    parser.add_argument('--num-workers', type=int, default=2)
+    args = parser.parse_args()
+
+    run(args)
